689e0db8a99478401a08fc15c644e57d15ff1663
Néstor Iriondo <nestoririondo@gmail.com>
2024-01-19 10:00:08 +0100

    bug fixes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,13 +6,15 @@ import "./Body.css";
 
 const MAX_QUESTIONS = 10;
 
-const getQuestions = async (setQuestions) => {
+const getQuestions = async (setQuestions, setLoading) => {
   try {
     const response = await axios.get(`${TRIVIA_API}&limit=${MAX_QUESTIONS}`);
     setQuestions(response.data);
     console.log(response.data);
   } catch (error) {
     console.log(error);
+  } finally {
+    setLoading(false)
   }
 };
 
@@ -20,27 +22,30 @@ const Body = () => {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState([]);
+  const [games, setGames] = useState(0)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    getQuestions(setQuestions);
-  }, []);
+    getQuestions(setQuestions, setLoading);
+  }, [games]);
 
   const handleRestart = () => {
+    setLoading(true)
     setCurrentQuestionIndex(0);
-    setAnswers([]);
-    getQuestions();
+    setAnswers([])
+    setGames(games+1)
   };
 
   return (
     <>
-      {questions.length > 0 && currentQuestionIndex !== MAX_QUESTIONS - 1 && (
+      {!loading && questions.length > 0 && currentQuestionIndex !== MAX_QUESTIONS && (
         <QuestionCard
           question={questions[currentQuestionIndex]}
           setCurrentQuestionIndex={setCurrentQuestionIndex}
           setAnswers={setAnswers}
         />
       )}
-        {currentQuestionIndex === 9 && (
+        {currentQuestionIndex === 10 && (
           <button onClick={handleRestart}>Restart</button>
         )}
       <div className="answers">
